1c32e74720a1fb38a9d4013ae42b358b87ea44c3
Daniel <daniel@cloudteam.ai>
2022-07-13 11:14:57 +0300

    added more azure connection types

--- ARM_templates/Deallocated_Stopped_VMS/Deallocating_Stopped_VMS.ps1
+++ ARM_templates/Deallocated_Stopped_VMS/Deallocating_Stopped_VMS.ps1
@@ -1,5 +1,47 @@
-Connect-AzAccount -TenantId "667d9faa-186f-4608-8a36-cf595f0350fb"
-$subs = Get-AzSubscription -TenantId "667d9faa-186f-4608-8a36-cf595f0350fb"
+param (
+    [Parameter ()]
+    [String] $ConnectionType = "managedidentity",
+    [Parameter ()]
+    [string] $ConnectionVarName = "",
+    [Parameter()]
+    [string] $TenantId
+)
+function ConnectToAzure {
+    param (
+        [Parameter()]
+        [string] $TenantId,
+        [Parameter()]
+        [string] $ConnectionVarName
+    )
+    if ($env:AUTOMATION_ASSET_ACCOUNTID) {
+        $ConnectionType = $ConnectionType.ToLower()
+        if ($ConnectionType -eq "managedidentity") {
+            Write-Output "----Identity connection-----"
+            Disable-AzContextAutosave -Scope Process | Out-Null
+            if ($ConnectionVarName) {
+                Write-Output "----User assigned----"
+                Connect-AzAccount -Identity -AccountId $(Get-AutomationVariable -Name $ConnectionVarName)
+            }
+            else {
+                Write-Output "----System assigned----"
+                Connect-AzAccount -Identity
+            }
+        }
+        elseif ($ConnectionType -eq "serviceprincipal") {
+            Write-Output "----Service connection----"
+            $runAsConnection = Get-AutomationConnection -Name 'AzureRunAsConnection' -ErrorAction Stop
+            Add-AzAccount -ServicePrincipal -Tenant $runAsConnection.TenantId -ApplicationId $runAsConnection.ApplicationId `
+                -CertificateThumbprint $runAsConnection.CertificateThumbprint -ErrorAction Stop | Out-Null 
+        }
+    }
+    else { Connect-AzAccount -TenantId $TenantId }
+}
+if (-not $(ConnectToAzure -TenantId $TenantId)) {
+    Write-Error "Failed to Connect to Azure."
+    exit 1
+}
+else { Write-Output "Successfully Connected to Azure." }
+$subs = Get-AzSubscription -WarningAction SilentlyContinue | Where-Object { $_.State.ToLower() -eq "enabled" }
 $count = 0
 foreach ($sub in $subs) {
     Set-AzContext $sub.Id
