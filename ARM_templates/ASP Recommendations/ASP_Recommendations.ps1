ba80956f4ddd4e9ffeba28ff0ed798cf30866ffe
Daniel <daniel@cloudteam.ai>
2022-07-13 14:04:13 +0300

    fixed asp

--- ARM_templates/ASP Recommendations/ASP_Recommendations.ps1
+++ ARM_templates/ASP Recommendations/ASP_Recommendations.ps1
@@ -5,24 +5,24 @@ Param
     [String] $AccountType = "ManagedIdentity",
     [Parameter(Mandatory = $false)]
     [String] $AccountName = "",
-    [Parameter (Mandatory = $false)]
+    [Parameter (Mandatory = $true)]
     [String] $StorageForLogID,
     [Parameter (Mandatory = $false)]
     [String] $logsName = "Converting_Service_Plan_SKU",
-    [Parameter (Mandatory = $false)]
+    [Parameter (Mandatory = $true)]
     [String] $ContainerName
 )
 
 
 <#
-    This function create the blob file inside the storage container
-    INPUT:  $logName - the log file name to enter
-            $ContainerName - the container name where the log is saved
-            $ctx - the context of the storage account
-            $LogSubjects - the subjects inside the csv log file
-    
-    OUTPUT: $Blob - the created blob for future writing
-#>
+     This function create the blob file inside the storage container
+     INPUT:  $logName - the log file name to enter
+             $ContainerName - the container name where the log is saved
+             $ctx - the context of the storage account
+             $LogSubjects - the subjects inside the csv log file
+     
+     OUTPUT: $Blob - the created blob for future writing
+ #>
 function createLogFile {
     param (
         $LogName,
@@ -45,10 +45,10 @@ function createLogFile {
 
 
 <#
-    this function connect to azure via Service Principal
-    INPUT: NONE
-    OUTPUT: NONE
-#>
+     this function connect to azure via Service Principal
+     INPUT: NONE
+     OUTPUT: NONE
+ #>
 function ConnectAsService {
     Write-Output "----Service connection----"
     $runAsConnection = Get-AutomationConnection -Name 'AzureRunAsConnection' -ErrorAction Stop
@@ -57,10 +57,10 @@ function ConnectAsService {
 }
 
 <#
- This function connect to azure as managed identity
- INPUT: NONE
- OUTPUT: NONE
-#>
+  This function connect to azure as managed identity
+  INPUT: NONE
+  OUTPUT: NONE
+ #>
 function ConnectAsIdentity {
     if ($AccountName) {
         $ID = Get-AutomationVariable -Name $AccountName
@@ -81,10 +81,10 @@ function ConnectAsIdentity {
 }
 
 <#
-    This function checks if ASP has sites or not
-    INPUT: $asp - the App Service plan
-    $OUTPUT: True if it has not sites and False if it has sites
-#>
+     This function checks if ASP has sites or not
+     INPUT: $asp - the App Service plan
+     $OUTPUT: True if it has not sites and False if it has sites
+ #>
 function CheckToDelete {
     param(
         $asp
@@ -97,10 +97,10 @@ function CheckToDelete {
 
 
 <#
-    This function check convertions options to App service plan
-    INPUT: $asp - the App Service plan
-    OUTPUT: Return a string that says the convertion option or empty string
-#>
+     This function check convertions options to App service plan
+     INPUT: $asp - the App Service plan
+     OUTPUT: Return a string that says the convertion option or empty string
+ #>
 function SwitchRecommendation {
     param (
         $asp
@@ -113,16 +113,16 @@ function SwitchRecommendation {
         "S3" { return "S3 -> P1v3" }
         default { return "" }
     }
-    
+
 }
 
 
 <#
-    This function returns the subscription, resource group and name of a storage account from a resource id
+     This function returns the subscription, resource group and name of a storage account from a resource id
 
-    INPUT: $saID - Storage account ID
-    OUTPUT: array of the data
-#>
+     INPUT: $saID - Storage account ID
+     OUTPUT: array of the data
+ #>
 function GetStorageAccountData {
     param(
         $saID
@@ -147,17 +147,15 @@ else {
 }
 
 #connecting to the storage account and creating the blob
-#$saData = GetStorageAccountData -saID $StorageForLogID
+$saData = GetStorageAccountData -saID $StorageForLogID
 Write-Output $saData
-$LogSubjects = "Resource Name,Resource Group,Subscription,Recommendation,Need To Delete?"
-$subs = Get-AzSubscription
-$logsName = $logsName + "_" + (Get-Date -Format "dd_MM_yyyy:HH_mm") + ".csv"
+$LogSubjects = "Resource Name,Resource Group,Subscription,Recommendation,Need To Delete?`n"
+$subs = Get-AzSubscription -TenantId "667d9faa-186f-4608-8a36-cf595f0350fb"
+$logsName = $logsName + "_" + (Get-Date -Format "dd_MM_yyyy: HH_mm") + ".csv"
 New-Item -Path . -Name $logsName
-Start-Sleep -Seconds 5
-Add-Content -Path "./$logsName" -Value $LogSubjects
-#Set-AzContext -SubscriptionId $saData[0] -WarningAction SilentlyContinue
-#$ctx = $(Get-AzStorageAccount -ResourceGroupName $saData[1] -Name $saData[2]).Context
-#$blob = createLogFile -LogName $logsName -ContainerName $ContainerName -ctx $ctx -LogSubjects $LogSubjects
+Set-AzContext -SubscriptionId $saData[0] -WarningAction SilentlyContinue
+$ctx = $(Get-AzStorageAccount -ResourceGroupName $saData[1] -Name $saData[2]).Context
+$blob = createLogFile -LogName $logsName -ContainerName $ContainerName -ctx $ctx -LogSubjects $LogSubjects
 #itterating over all the subscriptions
 foreach ($sub in $subs) {
     Set-AzContext -SubscriptionName $sub.Name -WarningAction SilentlyContinue
@@ -170,17 +168,15 @@ foreach ($sub in $subs) {
             #checking if the ASP has any sites
             if (CheckToDelete -asp $plan) {
                 #tagging for deletion and logging
-                #Update-AzTag -ResourceId $plan.Id -Tag @{"Candidate" = "DeleteASP" } -Operation Merge
-                #$blob.ICloudBlob.AppendText("$($plan.Name),$($r),$($sub.Name),X,V`n")
-                Add-Content -Path "./$($logsName)" -Value "$($plan.Name),$($r),$($sub.Name),X,V"
+                Update-AzTag -ResourceId $plan.Id -Tag @{"Candidate" = "DeleteASP" } -Operation Merge
+                $blob.ICloudBlob.AppendText("$($plan.Name),$($r),$($sub.Name),X,V`n")
             }
             #Checking if there is a recommendations for the plan
             elseif ($recommendation = SwitchRecommendation -asp $plan) {
                 #tagging for convertion and logging
-                #Update-AzTag -ResourceId $plan.Id -Tag @{"Candidate" = "Convert: $($recommendation)" } -Operation Merge
-                #$blob.ICloudBlob.AppendText("$($plan.Name), $($r),$($sub.Name),$($recommendation),X`n")
-                Add-Content -Path "./$($logsName)" -Value "$($plan.Name), $($r),$($sub.Name),$($recommendation),X"
+                Update-AzTag -ResourceId $plan.Id -Tag @{"Candidate" = "Convert: $($recommendation)" } -Operation Merge
+                $blob.ICloudBlob.AppendText("$($plan.Name), $($r),$($sub.Name),$($recommendation),X`n")
             }
         }
     }
-}
+} 
